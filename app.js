cdb09b0afebb291e724c491bf55ccda524f01656
Sean Fitzsimmons <seanfitzsimmons12@gmail.com>
2021-11-18 07:00:47 -0800

    set up app skeleton, campground and show page

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const path = require('path') 
 const mongoose = require('mongoose')
-const Campground = require('./models/campground')
+const Campground = require('./models/campground');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -21,10 +22,14 @@ app.get('/', (req, res) => {
     res.render('home') 
 })
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'cheap camping'})
-    await camp.save()
-    res.send(camp) 
+app.get('/campgrounds', async (req, res) => {
+   const campgrounds = await Campground.find({})
+   res.render('campgrounds/index', { campgrounds })
+})
+
+app.get('/campgrounds/:id', async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground})
 })
 
 app.listen(3000, () => {
